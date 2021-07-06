2099f509f7c6c8a92b214871a1a8d70df3b397c5
Neha <nehagupta2371@gmail.com>
2021-07-06 21:33:04 +0530

    Version1

---
+++ public/script.js
@@ -0,0 +1,289 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+
+    path: '/peerjs',
+    host: '/',
+    port: '443'
+})
+let temp;
+let myVideoStream;
+var activeSreen = "";
+const myVideo = document.createElement('video')
+myVideo.muted = true;
+const peers = {}
+const peerscall = {}
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream, temp)
+
+    myPeer.on('connection', function(conn) {
+        var uniId = conn.peer
+        peers[uniId] = conn;
+        conn.on('close', () => {
+
+            console.log("conn close event 1");
+            handlePeerDisconnect(document.getElementById(uniId));
+            conn.peerConnection.close();
+
+
+        })
+
+    });
+
+    myPeer.on('call', call => {
+        peerscall[call.peer] = call;
+        call.answer(stream)
+        var video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+
+            addVideoStream(video, userVideoStream, call.peer)
+        });
+
+
+    });
+    document.getElementById("shareScreen").addEventListener('click', (e) => {
+        let enabled = document.getElementById("shareScreen").classList.contains("active-btn");
+        if (enabled) {
+            document.getElementById("shareScreen").classList.remove("active-btn");
+            stopScreenShare();
+        }
+        else {
+              navigator.mediaDevices.getDisplayMedia({
+                video: {
+                    cursor: 'always'
+                },
+                audio: {
+                    echoCancellation: true,
+                    noiseSupprission: true
+                }
+              }).then(stream => {
+                document.getElementById("shareScreen").classList.add("active-btn");
+          
+                let videoTrack = stream.getVideoTracks()[0];
+                videoTrack.onended = function () {
+                     document.getElementById("shareScreen").classList.remove("active-btn");
+                     stopScreenShare();
+                }
+                Object.keys(peerscall).forEach(function (x) {
+                    let sender = peerscall[x].peerConnection.getSenders().find(function (s) {
+                        return s.track.kind == videoTrack.kind;
+                    })
+                    sender.replaceTrack(videoTrack);
+                })
+            }).catch((err) => {
+                console.log("unable to get display media" + err)
+            })
+
+        }
+
+    });
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+
+    let text = $("input");
+
+    $('html').keydown(function(e) {
+        if (e.which == 13 && text.val().length !== 0) {
+            socket.emit('message', text.val());
+             $(".messages").append(`<li class="message user_message"><b>Me</b><br/>${message}</li>`);
+            scrollToBottom()
+            text.val('')
+        }
+    });
+    socket.on("createMessage", message => {
+        $(".messages").append(`<li class="message"><b>Anonymous</b><br/>${message}</li>`);
+        scrollToBottom()
+    })
+})
+
+function stopScreenShare() {
+        let videoTrack = myVideoStream.getVideoTracks()[0];
+        Object.keys(peerscall).forEach(function(x) {
+            let sender = peerscall[x].peerConnection.getSenders().find(function(s) {
+                return s.track.kind == videoTrack.kind;
+            })
+            sender.replaceTrack(videoTrack);
+        })
+    }
+
+myPeer.on('open', id => {
+    temp = id;
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+    console.log("I am ");
+    console.log(temp);
+    console.log("calling ");
+    console.log(userId);
+    const conn = myPeer.connect(userId, {
+        metadata: {
+            uniId: temp
+        }
+    });
+    const call = myPeer.call(userId, stream)
+    var video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream, userId)
+    })
+
+    conn.on('close', () => {
+        console.log("conn close event 2");
+        handlePeerDisconnect(video);
+        conn.close();
+    });
+    peerscall[userId] = call;
+    peers[userId] = conn;
+
+}
+
+function addVideoStream(video, stream, userId) {
+
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    video.id = userId
+    videoGrid.append(video)
+
+}
+
+
+
+
+const scrollToBottom = () => {
+    var d = $('.main__chat_window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+
+const muteUnmute = () => {
+    let enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+const playStop = () => {
+
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+
+
+const setMuteButton = () => {
+    const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+    const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const leaveMeeting = () => {
+
+    location.href = "http://www.google.com";
+}
+
+const isHidden = (screen) => screen.classList.contains("screen-hide");
+
+const handleScreen = (screen) => {
+    const left_container = document.querySelector(".main__left");
+    const right_container = document.querySelector(".main__right");
+    const chatScreen = document.getElementById("chat-screen");
+    const usersScreen = document.getElementById("users-screen");
+
+    if (screen.id === "chats") {
+        handleActive("chat-btn");
+        if (activeSreen === "") {
+            chatScreen.classList.remove("screen-hide");
+            activeSreen = "chats";
+        } else if (activeSreen === "chats") {
+            chatScreen.classList.add("screen-hide");
+            activeSreen = "";
+        } else {
+            chatScreen.classList.remove("screen-hide");
+            usersScreen.classList.add("screen-hide");
+            activeSreen = "chats";
+            handleActive("users-btn");
+        }
+    } else {
+        handleActive("users-btn");
+        if (activeSreen === "") {
+            usersScreen.classList.remove("screen-hide");
+            activeSreen = "users";
+        } else if (activeSreen === "users") {
+            usersScreen.classList.add("screen-hide");
+            activeSreen = "";
+        } else {
+            usersScreen.classList.remove("screen-hide");
+            chatScreen.classList.add("screen-hide");
+            activeSreen = "users";
+            handleActive("chat-btn");
+        }
+    }
+
+    if (isHidden(right_container)) {
+        right_container.classList.remove("screen-hide");
+        left_container.classList.remove("screen-full");
+    } else if (activeSreen === "") {
+        right_container.classList.add("screen-hide");
+        left_container.classList.add("screen-full");
+    }
+};
+
+const handleActive = (buttonClass) => {
+    const button = document.querySelector(`.${buttonClass}`);
+    const active = button.classList.contains("active-btn");
+
+    if (active) button.classList.remove("active-btn");
+    else button.classList.add("active-btn");
+};
+
+function handlePeerDisconnect(video) {
+
+    video.srcObject = null;
+    console.log("left " + video.id);
+    video.remove();
+
+}
