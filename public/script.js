5855af9745a6d9157b24dd0e1f841fe57fab316b
Neha <nehagupta2371@gmail.com>
2021-07-06 23:41:45 +0530

    new

--- public/script.js
+++ public/script.js
@@ -173,9 +173,9 @@ const muteUnmute = () => {
     let enabled = myVideoStream.getAudioTracks()[0].enabled;
     if (enabled) {
         myVideoStream.getAudioTracks()[0].enabled = false;
-        setUnmuteButton();
+        document.querySelector('.main__mute_button').innerHTML =`<i class="unmute fas fa-microphone-slash"></i><span>Unmute</span>`
     } else {
-        setMuteButton();
+        document.querySelector('.main__mute_button').innerHTML =  `<i class="fas fa-microphone"></i><span>Mute</span>`
         myVideoStream.getAudioTracks()[0].enabled = true;
     }
 }
@@ -185,42 +185,11 @@ const playStop = () => {
     let enabled = myVideoStream.getVideoTracks()[0].enabled;
     if (enabled) {
         myVideoStream.getVideoTracks()[0].enabled = false;
-        setPlayVideo()
+        document.querySelector('.main__video_button').innerHTML = `<i class="stop fas fa-video-slash"></i><span>Play Video</span>`
     } else {
         document.querySelector('.main__video_button').innerHTML=`<i class="fas fa-video"></i><span>Stop Video</span>`
         myVideoStream.getVideoTracks()[0].enabled = true;
     }
-}
-
-
-
-const setMuteButton = () => {
-    const html = `
-    <i class="fas fa-microphone"></i>
-    <span>Mute</span>
-  `
-    document.querySelector('.main__mute_button').innerHTML = html;
-}
-
-const setUnmuteButton = () => {
-    const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
-  `
-    document.querySelector('.main__mute_button').innerHTML = html;
-}
-
-// const setStopVideo = () => {
-//     const html = 
-//      = html;
-// }
-
-const setPlayVideo = () => {
-    const html = `
-  <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
-  `
-    document.querySelector('.main__video_button').innerHTML = html;
 }
 
 const leaveMeeting = () => {
