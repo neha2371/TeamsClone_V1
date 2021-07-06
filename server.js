4ff7f3e1888686aac38b84f7ea1d7cc35738df4e
Neha <nehagupta2371@gmail.com>
2021-07-06 22:01:22 +0530

    new

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
 
         socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId);
+        socket.to(roomId).emit('user-connected', userId);
        
         socket.on('message', (message) => {
             
-            io.to(roomId).broadcast.emit('createMessage', message)
+            io.to(roomId).emit('createMessage', message)
         });
 
         socket.on('disconnect', () => {
