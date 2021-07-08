795bb3a573d5e4eec1c3d4573244708cd260295c
Neha <nehagupta2371@gmail.com>
2021-07-08 15:48:21 +0530

    new

--- public/script.js
+++ public/script.js
@@ -244,7 +244,7 @@ const videoOnOff = () => {
                     document.getElementById("incVideo").innerHTML=`<i class="fas fa-video"></i>Play Incoming Video`
                     var elem = document.getElementsByTagName("video");
                     elem.forEach(function (vidEl) {
-                        console.log(vidE1.id)
+                        console.log(vidEl.id)
                         if(vidEl.id != "self")
                         stopStreamedVideo(vidEl);
                     })
