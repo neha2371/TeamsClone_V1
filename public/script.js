2961974e86c5758bb3f3559bb7aa4742b1ec6ff5
Neha <nehagupta2371@gmail.com>
2021-07-08 14:11:14 +0530

    new

--- public/script.js
+++ public/script.js
@@ -40,6 +40,7 @@ navigator.mediaDevices.getUserMedia({
         call.answer(stream)
         var video = document.createElement('video')
         call.on('stream', userVideoStream => {
+            if(document.getElementById("incVideo").classList.contains("active-btn") == false)
             addVideoStream(video, userVideoStream, call.peer)
             document.getElementById("incVideo").addEventListener("click", () => {
                 videoOnOff(video, userVideoStream);
@@ -169,6 +170,7 @@ function connectToNewUser(userId, stream) {
     const call = myPeer.call(userId, stream)
     var video = document.createElement('video')
     call.on('stream', userVideoStream => {
+        if(document.getElementById("incVideo").classList.contains("active-btn") == false)
         addVideoStream(video, userVideoStream, userId)
          document.getElementById("incVideo").addEventListener("click", () => {
                 videoOnOff(video, userVideoStream);
