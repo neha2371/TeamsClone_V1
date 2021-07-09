7b0c833efd71952695b447f2e69aee5bde6ecb85
Neha <nehagupta2371@gmail.com>
2021-07-09 14:06:38 +0530

    version2.0

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
-
+    //res.redirect(`/${uuidV4()}`)
+    res.render('homepage')
 })
 
 app.get('/:room', (req, res) => {
